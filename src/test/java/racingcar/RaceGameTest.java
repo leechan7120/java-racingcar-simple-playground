bc9ee544519881a0ca2a6de40e632c83fda556ec
leechan7120 <chanhyoung7120@naver.com>
2026-03-26 10:46:04 +0900

    fix : Test 중복 삭제

--- src/test/java/racingcar/RaceGameTest.java
+++ src/test/java/racingcar/RaceGameTest.java
@@ -4,15 +4,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Assumptions.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RacingCarRaceTest {
+public class RaceGameTest {
     @DisplayName("우승자 확인")
     @Test
     void WinnerTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
         String[] carNames = {"Adam", "Bro", "July"};
         race.setCars(carNames);
         race.carsInRacing[0].distanceFromStart = 3;
@@ -24,56 +23,45 @@ public class RacingCarRaceTest {
         String[] expected = {"Adam", "July"};
 
         assertArrayEquals(expected, winners);
-
     }
 
     @DisplayName("이름 분리가 잘 되는지 확인")
     @Test
     void splitNameTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
         String str = "Joe,Mist,Aio";
 
         String[] actual = race.splitName(str);
         String[] expected = {"Joe", "Mist", "Aio"};
 
         assertArrayEquals(expected, actual);
-
-    }
-
-    @DisplayName("이름이 5글자 초과일 경우 예외를 제대로 throw하는지 확인")
-    @Test
-    void validationTest() {
-        RacingcarRace race = new RacingcarRace();
-        String name = "Micheal";
-
-        assertThrows(IllegalArgumentException.class, () -> race.checkNameValidation(name));
-
     }
 
     @DisplayName("자동차가 게임에 제대로 세팅되는지 확인")
     @Test
     void setCarsTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
         String[] carNames = {"Joe", "Mist", "Aio"};
         race.setCars(carNames);
+
         assertEquals("Joe", race.carsInRacing[0].carName);
         assertEquals("Mist", race.carsInRacing[1].carName);
         assertEquals("Aio", race.carsInRacing[2].carName);
-
     }
 
-    @DisplayName("제대로 된 이름이 아닐 경우 오류를 검출하는지 확인")
+    @DisplayName("이름이 5글자 이하가 아닐 경우 오류를 검출하는지 확인")
     @Test
     void setCarsErrorTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
         String[] carNames = {"Joe", "Mister", "Aio"};
+
         assertThrows(IllegalArgumentException.class, () -> race.setCars(carNames));
     }
 
     @DisplayName("출력이 제대로 이루어지는지 확인")
     @Test
     void printTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
         String[] names = {"Miro"};
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream original = System.out;
@@ -90,7 +78,7 @@ public class RacingCarRaceTest {
     @DisplayName("승자가 제대로 출력되는지 확인")
     @Test
     void printWinnerTest() {
-        RacingcarRace race = new RacingcarRace();
+        raceGame race = new raceGame();
 
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream original = System.out;
@@ -106,8 +94,5 @@ public class RacingCarRaceTest {
 
         assertEquals("Adam, July(이)가 최종 우승했습니다.", outputStream.toString());
         System.setOut(original);
-
-
     }
-
 }
